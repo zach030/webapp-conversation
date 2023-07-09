209742329a7e2dd9cd71bc8f0569d205bf5f9603
zach <zachchou016@gmail.com>
2023-07-09 17:50:30 +0800

    update key

--- config/index.ts
+++ config/index.ts
@@ -1,13 +1,13 @@
 import type { AppInfo } from '@/types/app'
 export const API_URL = ''
-if (!process.env.APP_ID) {
+if (!process.env.NEXT_PUBLIC_APP_ID) {
   throw new Error("Missing env var from OpenAI");
 }
-export const APP_ID = process.env.APP_ID
-if (!process.env.API_KEY) {
+export const APP_ID = process.env.NEXT_PUBLIC_APP_ID
+if (!process.env.NEXT_PUBLIC_API_KEY) {
   throw new Error("Missing env var from OpenAI");
 }
-export const API_KEY = process.env.API_KEY
+export const API_KEY = process.env.NEXT_PUBLIC_API_KEY
 export const APP_INFO: AppInfo = {
   title: 'Chat APP',
   description: '',
